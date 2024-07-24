9265c513942b244e8e9b6beb76fe452a241e47dc
Gautam Verma <95125302+KHO0Ni@users.noreply.github.com>
2024-07-24 15:51:08 +0530

    Add files via upload

---
+++ median of the NumPy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create a NumPy array with an odd number of elements
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# Compute the median of the array
+median_value = np.median(x_odd)
+
+# Print the median value
+print(median_value)
+'''
+Out:put
+PS C:\Users\verma> & C:/Users/verma/AppData/Local/Programs/Python/Python311/python.exe "d:/verma/2 Documents/Anudip/python/median of the NumPy.py"
+4.0
+'''
---
+++ standard deviation .py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create the NumPy array with the specified elements
+arr = np.array([20, 2, 7, 1, 34])
+
+# Compute the standard deviation of the array
+std_deviation = np.std(arr)
+
+# Print the standard deviation value
+print(std_deviation)
+'''
+Output:
+"d:/verma/2 Documents/Anudip/python/standard deviation .py"
+12.576167937809991
+'''
